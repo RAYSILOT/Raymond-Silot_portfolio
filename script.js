c206a8b0b5383655a7c450fd98d97591113d841c
RAYSILOT <160706045+RAYSILOT@users.noreply.github.com>
2026-07-07 13:02:13 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// GSAP Animations
+gsap.from("#about h1", {duration: 1, y: -50, opacity: 0});
+gsap.from("#about p", {duration: 1, y: 50, opacity: 0, delay: 0.5});
+gsap.from("#projects .card", {duration: 1, y: 50, opacity: 0, stagger: 0.3});
+gsap.from("#contact h1", {duration: 1, scale: 0.8, opacity: 0, delay: 0.5});
